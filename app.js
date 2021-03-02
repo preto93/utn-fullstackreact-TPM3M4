966dca18282ff1800d9631160272740596bf5475
Claudio Garcia Iglesia <claudio@fsdev.wonderbyte>
2021-03-02 01:04:37 -0300

    remove auth files and code

--- app.js
+++ app.js
@@ -1,47 +1,16 @@
 // Se utiliza para el promosify, transforma callbacks en promise para utilizar con async await
 const util = require('util'); 
 
-// Se utiliza para manejar los token de autenticacion
-const jwt = require('jsonwebtoken'); 
-
-// Se utiliza para que el middleware de autenticacion no se ejecute en las rutas de login y register
-const unless = require('express-unless'); 
-
-// Se utiliza para encriptar la clave provista por el usuario con el fin de guardarla encriptada en la base
-// De esta forma quien tenga acceso a la base, no tendra acceso a la clave
-const bcrypt = require('bcrypt');
-
-// Se utiliza para que Node lea las variables de entorno del archivo oculto .env
-const dotenv = require('dotenv').config();
-
-
 // Configuracion de express
 const express = require('express');
 const app = express();
 
-// Se creo un modulo para separar el codigo del middleware en otro archivo
-// __dirname se utiliza para que cuando subamos el programa a un servidor, el mismo acceda correctamente
-// a las rutas donde se encuentran los archivos
-const { auth } = require(__dirname + '/auth');
-
 // Express recibira y enviara las solicitudes en formato JSON
 app.use(express.json());
-
-// Se configura auth para utilizar unless
-auth.unless = unless;
-
-// Se configuran las rutas excluidas que no seran afectadas por el middleware auth
-app.use(auth.unless({
-    path: [
-        { url: '/login', methods: ['POST'] },
-        { url: '/registro', methods: ['POST'] },
-    ]
-}));
 
 // Si el programa se ejecuta en un servidor, el puerto lo asignara una variable de entorno
 // Caso contrario se utilizara el puerto 3000
 const PORT = process.env.PORT || 3000;
-
 
 // Configuracion mysql
 const mysql = require('mysql');
